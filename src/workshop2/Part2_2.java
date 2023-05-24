fb82571f7b172caa80c924262ca1d1539bbeaaff
HoanDo95 <hoandshe170447@fpt.edu.vn>
2023-05-24 21:29:23 +0700

    Initial commit

---
+++ src/workshop2/Part2_2.java
@@ -0,0 +1,30 @@
+
+package workshop2;
+import java.util.Scanner;
+
+public class Part2_2 {
+    public String inputString() throws Exception{
+        String pattern="SE1830";
+        String s ="";
+        Scanner sc=new Scanner(System.in);
+        System.out.print("input the string:");
+        s=sc.nextLine();
+        if (!s.matches(pattern))
+            throw new Exception();
+        return s;
+    }
+    public static void main(String [] args){
+        Part2_2 obj=new Part2_2();
+        boolean cont=false;
+        do{
+            try{
+                String s=obj.inputString();
+                System.out.println("the string is "+s);
+                cont = false;
+            }catch(Exception e){
+                System.out.println("The string is invalid");
+                cont = true;
+            }
+        }while(cont);
+    }
+}
